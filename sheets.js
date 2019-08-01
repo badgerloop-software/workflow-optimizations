71c74c18cb08b3d236bd47aab7dba8d8bb5775ce
Ezra Boley <eboley@wisc.edu>
2019-07-31 21:56:19 -0500

    Adding the beginning of a slack component and date checking to script

--- sheets.js
+++ sheets.js
@@ -1,6 +1,6 @@
 // General structure
 
-STATUSES = ["active", "backlog", "complete", "cancelled"];
+STATUSES = ["Active", "Backlog", "Complete", "Cancelled"];
 TEAM     = ["Electrical", "Mechanical", "Operations"];
 MIME     = "application/json";
 
@@ -8,21 +8,57 @@ testMsg =
   { 
     parameter: 
     {
-      dueDate: "1/20/19",
-      createDate: "1/13/19",
+      dueDate: "2019-09-10",
       re: "ezra",
       subTeam: "controls",
       group: "Electrical",
       name: "Make Task List",
       notes: "nothing",
-      status: "active"
+      status: "Active"
     }
   };
 
 function unitTest() {
-  console.log(doGet(testMsg));
+  console.log(doPost(testMsg));
 }
 
+function getCurrDay() {
+  var today = new Date();
+  
+  var dd = String(today.getDate()).padStart(2, '0');
+  var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  var yyyy = today.getFullYear();
+
+  return (yyyy + '-' + mm + '-' + dd);  
+}
+
+function compareDates(createDate, dueDate) {
+  var currDateArr = createDate.split("-",3);
+  var dueDateArr  = dueDate.split("-",3);
+  
+  /* YYYY - MM - DD */
+  
+  if (currDateArr[0] < dueDateArr[0]) /* Its a later year, we don't need to check anything else */
+  {
+    return true;
+  }
+  else if (currDateArr[0] === dueDateArr[0])  /* Same year */ 
+  {
+    if (currDateArr[1] < dueDateArr[1])  /* Its a later month, we're good to go */
+    {
+      return true;
+    } 
+    else if (currDateArr[1] === dueDateArr[1])  /* Same month */
+    {
+      if (currDateArr[2] < dueDateArr[2]) 
+      {
+        return true;
+      }
+    }
+  }
+  
+  return false;  /* Create date is later than due date */
+}
 
 function buildMsg(msg) {
   var name = msg.name;
@@ -55,6 +91,7 @@ function parse(msg) {
   
   if (row === undefined)
     return undefined;
+
   
   sheet.appendRow(row);
 
@@ -62,6 +99,7 @@ function parse(msg) {
 }
 
 function doGet(e) {
+  setup();
   var params = e.parameter; // Grab the parameters sent by slack
   return parse(params); //I have no how to validate?
 }
@@ -69,10 +107,13 @@ function doGet(e) {
 function doPost(e) {
   console.log("SAW POST");
   console.log(e);
-  if (e.postData.type !== MIME) return;
-  var params = JSON.parse(e.postData.contents);
-  
-  parse(params); // POST is bullshit anyway
+  setup();
+  /* if (e.postData.type !== MIME) return; */
+  /*var params = JSON.parse(e.postData.contents);*/
+  var params = e.parameter;
+
+  params.createDate = getCurrDay();  /* I have too much of the structure done, just toss it in there */
+  parse(params); 
 }
 
 
@@ -86,9 +127,16 @@ function validate(msg) {
   var re = msg.re;
   var notes = msg.notes;
   
+  
+  
   /* TODO We need to validate dates better */
  // if (dueDate < createDate)
  //   return false;
+  
+  if (!compareDates(createDate, dueDate)) {
+    console.log("Invalid dates");
+    return false;
+  }
   
   if (STATUSES.indexOf(status) < 0) {
     console.log("Failed here1\n");
@@ -100,5 +148,25 @@ function validate(msg) {
     return false; 
   }
   return true;
+}
+
+function setup() {
+  // https://github.com/uxitten/polyfill/blob/master/string.polyfill.js
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
+  if (!String.prototype.padStart) {
+    String.prototype.padStart = function padStart(targetLength, padString) {
+        targetLength = targetLength >> 0; //truncate if number, or convert non-number to 0;
+        padString = String(typeof padString !== 'undefined' ? padString : ' ');
+        if (this.length >= targetLength) {
+            return String(this);
+        } else {
+            targetLength = targetLength - this.length;
+            if (targetLength > padString.length) {
+                padString += padString.repeat(targetLength / padString.length); //append to original to ensure we are longer than needed
+            }
+            return padString.slice(0, targetLength) + String(this);
+        }
+    };
+}
 }
 
